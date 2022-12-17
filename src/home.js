0e5bd8181bd0ff496b2b2974cdde4f9853aa9102
Juhreisf <109034395+Juhreisf@users.noreply.github.com>
2022-12-16 21:49:16 -0300

    botões barra de navegação

--- src/home.js
+++ src/home.js
@@ -1,9 +1,21 @@
 export default () => {
-    const container = document.createElement('div');
+  const container = document.createElement('div');
 
-    const template = `
-       <h1 class="filmes">Filmes</h1>
+  const template = `
+      <nav class="filmes-catg">
+        <ul>
+          <li class="btn-filmes">
+              <a href="/#home" class="link">Ação</a>
+          </li>
+          <li class="btn-filmes">
+              <a href="/#home" class="link">Animação</a>
+          </li>
+          <li class="btn-filmes">
+              <a href="/#home" class="link">Romance</a>
+          </li>
+        </ul>
+      </nav>
      `;
-     container.innerHTML = template;
-     return container;
+  container.innerHTML = template;
+  return container;
 }
